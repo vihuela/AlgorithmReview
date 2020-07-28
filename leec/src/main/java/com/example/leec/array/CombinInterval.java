052e8ce133e33568abd82c4b35f88b96d6eabd48
rickyyao <lomoliger@hotmail.com>
2020-07-28 23:33:10 +0800

    add

--- leec/src/main/java/com/example/leec/array/CombinInterval.java
+++ leec/src/main/java/com/example/leec/array/CombinInterval.java
@@ -8,27 +8,32 @@ import java.util.Comparator;
 
 public class CombinInterval {
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{15, 18}, {2, 6}, {1, 3}, {8, 10}};
+        int[][] arr = new int[][]{{15, 18}, {2, 6}, {1,3}, {8, 10}};
         PrintUtil.printArr(merge(arr));
     }
 
     public static int[][] merge(int[][] intervals) {
-
+        // 先按照区间起始位置排序
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] t1, int[] t2) {
                 return t1[0] - t2[0];
             }
         });
+
+        // 遍历区间
         int[][] res = new int[intervals.length][2];
-        int row = -1;
-        for (int[] item : intervals) {
-            if (row == -1 || item[0] > intervals[row][1]) {
-                res[++row] = item;
+        int idx = -1;
+        for (int[] interval: intervals) {
+            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
+            // 则不合并，直接将当前区间加入结果数组。
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
             } else {
-                res[row][1] = Math.max(res[row][1], item[1]);
+                // 反之将当前区间合并至结果数组的最后区间
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
             }
         }
-        return Arrays.copyOf(res, row + 1);
+        return Arrays.copyOf(res, idx + 1);
     }
 }
