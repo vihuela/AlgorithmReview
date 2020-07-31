d02a5db9c934b04953e78c39683fe3bc5325a3ac
rickyyao <rickkyao@xiaoe-tech.com>
2020-07-31 16:43:04 +0800

    demo add

---
+++ leec/src/main/java/com/example/leec/string/CommonPrefix.java
@@ -0,0 +1,28 @@
+package com.example.leec.string;
+
+
+public class CommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        //初始化首个字符串
+        String ans = strs[0];
+        //从第二个字符串开始遍历
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if (ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
+
+}
---
+++ leec/src/main/java/com/example/leec/string/LongestPalindrome.java
@@ -0,0 +1,35 @@
+package com.example.leec.string;
+
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("fbbf"));
+    }
+
+    public static String longestPalindrome(String s) {
+        String ans = "";
+        int max = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j <= len; j++) {
+                String test = s.substring(i, j);
+                if (isPalindromic(test) && test.length() > max) {
+                    ans = s.substring(i, j);
+                    max = Math.max(max, ans.length());
+                }
+            }
+        return ans;
+
+    }
+
+    public static boolean isPalindromic(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
