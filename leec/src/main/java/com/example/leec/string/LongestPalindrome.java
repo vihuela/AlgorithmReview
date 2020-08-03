62abc7ff28578ce57b88decf132247deea838144
rickyyao <lomoliger@hotmail.com>
2020-08-03 23:51:30 +0800

    add

--- leec/src/main/java/com/example/leec/string/LongestPalindrome.java
+++ leec/src/main/java/com/example/leec/string/LongestPalindrome.java
@@ -7,25 +7,26 @@ public class LongestPalindrome {
     }
 
     public static String longestPalindrome(String s) {
-        String ans = "";
-        int max = 0;
-        int len = s.length();
-        for (int i = 0; i < len; i++)
-            for (int j = i + 1; j <= len; j++) {
-                String test = s.substring(i, j);
-                if (isPalindromic(test) && test.length() > max) {
-                    ans = s.substring(i, j);
-                    max = Math.max(max, ans.length());
+
+        int maxLen = 0;
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String check = s.substring(i, j);
+                if (isPalindromic(check) && check.length() > maxLen) {
+                    res = s.substring(i, j);
+                    maxLen = Math.max(maxLen, res.length());
                 }
             }
-        return ans;
-
+        }
+        return res;
     }
 
     public static boolean isPalindromic(String s) {
-        int len = s.length();
-        for (int i = 0; i < len / 2; i++) {
-            if (s.charAt(i) != s.charAt(len - i - 1)) {
+        //abba
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }
         }
